683255b077ef8315b3854ee0b06aca4f75ba1567
nadhiroov <nadhirov@gmail.com>
2024-05-07 08:33:48 +0700

    6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,45 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id'    => 1,
+            'slug'  => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Nadhir Barabud',
+            'body'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, veritatis nostrum. Sint temporibus dicta blanditiis iusto, excepturi velit minus delectus dolorem! Fuga pariatur quae voluptatem recusandae. Aperiam distinctio adipisci delectus!'
+        ], [
+            'id'    => 2,
+            'slug'  => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Nadhir Barabud',
+            'body'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugit autem enim quia debitis. Distinctio in quas quam magnam corporis ea commodi amet iure, dolores laboriosam accusantium repellendus fugit? Distinctio'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id'    => 1,
+            'slug'  => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Nadhir Barabud',
+            'body'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, veritatis nostrum. Sint temporibus dicta blanditiis iusto, excepturi velit minus delectus dolorem! Fuga pariatur quae voluptatem recusandae. Aperiam distinctio adipisci delectus!'
+        ], [
+            'id'    => 2,
+            'slug'  => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Nadhir Barabud',
+            'body'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugit autem enim quia debitis. Distinctio in quas quam magnam corporis ea commodi amet iure, dolores laboriosam accusantium repellendus fugit? Distinctio'
+        ]
+    ];
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+    
+    return view('post', ['title' => 'Single post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
