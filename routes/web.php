d6920d7803179e95711a26359f34d7c7ae199d13
nadhiroov <nadhirov@gmail.com>
2024-05-14 09:58:50 +0700

    9. Eloquent ORM & Post Model

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    $post = Post::find($slug);    
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single post', 'post' => $post]);
 });
 
